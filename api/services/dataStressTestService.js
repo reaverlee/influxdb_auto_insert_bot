cb2eb5550459b2141679230d53f12cd2ec60b6d6
Jjunjiii’ <drum1701@gmail.com>
2023-10-18 10:14:20 +0900

    modify code

--- api/services/dataStressTestService.js
+++ api/services/dataStressTestService.js
@@ -32,40 +32,6 @@ const insertData = async () => {
   }
 };
 
-// const intervalTest = setInterval(insertData, 100);
-
-// const dataStressTest = async () => {
-//   try {
-//     const temp = await random(17, 23);
-//     const humi = await random(61, 66);
-//     const THI = await random(1, 5);
-//     const pm10 = await random(7, 12);
-//     const pm25 = await random(7, 12);
-//     const pm1 = await random(7, 12);
-//     const co2 = await random(1066, 1071);
-//     const tvoc = await random(112, 118);
-//     const o2 = await random(16, 21);
-
-//     await dataStressTestDao.insertData(
-//       temp,
-//       humi,
-//       THI,
-//       pm10,
-//       pm25,
-//       pm1,
-//       co2,
-//       tvoc,
-//       o2
-//     );
-//     // setInterval(() => insertData(), 100);
-//     const message = { message: "Success Insert Data " };
-//     return message;
-//   } catch (error) {
-//     console.error("Error:", error);
-//     throw error;
-//   }
-// };
-
 module.exports = {
   startInterval: () => {
     if (!intervalTest) {
